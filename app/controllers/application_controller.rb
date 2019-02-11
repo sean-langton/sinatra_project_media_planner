06cbf9d94b36395feb91e266b0c9f34d6b73dcf3
github username <github email address>
2019-02-11 01:58:53 -0500

    Establish Layout & CSS File

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,10 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     enable :sessions
-    set :session_secret, "fdsakl;fja343qrejdjasklfjasl;fase23423qrljsdflk;asdjfkl;asjrio3w;rjdfsan"
+    set :session_secret, "87827cb2ab34ec45aff97fb396dcfe34e6be4a2564c8047ffacd3fa8e519f0cf19a277feefc59a45c02594ac51d83362c1921a38168575ea8fb65c3d8485a232"
     set :views, 'app/views'
   end
-  
+
   get "/" do
     erb :index
   end
