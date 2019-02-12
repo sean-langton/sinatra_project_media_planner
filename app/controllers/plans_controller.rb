d9e1d8eacfd2e186eaf3d4f7fe7c8a80e48744a7
github username <github email address>
2019-02-12 18:50:26 -0500

    -Controller for Creating Plans and Channels

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -11,8 +11,15 @@ class PlansController < ApplicationController
   post "/plans" do
     if Helpers.is_logged_in?(session)
       @user = Helpers.current_user(session)
-#      @plan = Plan.create(params) unless params[:plan_name].blank? || params[:plan_budget]
-      puts params
+      @plan = Plan.create(params[:plan]) unless params[:plan][:plan_name].blank? || params[:plan][:plan_budget].blank?
+      @user.plans << @plan
+      params[:channel].each do |channel|
+        if !channel["channel_name"].blank? && !channel["channel_budget"].blank?
+            new_channel = Channel.create(channel)
+            @plan.channels << new_channel
+        end
+      end
+        binding.pry      
       redirect to "/#{@user.slug}/index"
     else
       redirect to "/login"
