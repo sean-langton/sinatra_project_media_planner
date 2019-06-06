4c56447361b415ad23f3434933851601ab5cafbc
github username <github email address>
2019-06-06 14:18:39 -0400

    Removal of SQLite

--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,12 @@
-ENV['SINATRA_ENV'] ||= "development"
+configure :production do
+   db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
-require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
-
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
-
-require_all 'app'
+   ActiveRecord::Base.establish_connection(
+     :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+     :host     => db.host,
+     :username => db.user,
+     :password => db.password,
+     :database => db.path[1..-1],
+     :encoding => 'utf8'
+     )
+end
